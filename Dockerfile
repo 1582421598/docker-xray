f05668aabe5505d5c72dec0dfd79bde256262009
1582421598 <26507452+1582421598@users.noreply.github.com>
2023-10-06 18:31:05 +0800

    优化脚本兼容性

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:22.04 as builder
+FROM ubuntu:22.04
 
 MAINTAINER 1582421598
 
--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
-mkdir Internet
-cd Internet
+#!/bin/bash
+BASE_DIR=$(dirname "$0")
+mkdir "$BASE_DIR/Internet"
+cd "$BASE_DIR/Internet"
 wget https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
 unzip Xray-linux-64.zip -d xray
 sudo docker build -f ../Dockerfile -t xray-core:latest .
-sudo docker run -p 80:80 -p 443:443 --name xray --volume ~/docker-xray/Internet/xray:/app/download/xray-core -d xray-core:latest
+sudo docker run -p 11111:11111 -p 22222:22222 --name xray --volume "$BASE_DIR/Internet/xray:/app/download/xray-core" -d xray-core:latest
